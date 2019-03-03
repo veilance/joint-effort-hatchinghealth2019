03980afe9c30ca8f826b9508cbb8ef8bd24b3087
Gordon Yeh <gordon.yeh.dev@gmail.com>
2019-03-03 11:41:22 -0800

    make main screen default

--- demos/index.js
+++ demos/index.js
@@ -1,21 +1,16 @@
-import { ModelTrainingYielder } from "@tensorflow/tfjs-layers/dist/base_callbacks";
-
 $('#start-session').on('click', function() {
-    console.log('start-session: clicked');
     $('#main-screen').addClass('screen-inactive');
     $('#session-screen').removeClass('screen-inactive');
 
     $('#exercise-1').click(function() {
         console.log("Ex1 clicked");
         $('#myModal').style.display = "block";
-    })
+    });
 });
 
-$('button.return-to-main').array.forEach((btn) => {
-    btn.on('click', function() {
-        $('#main-screen').removeClass('screen-inactive');
-        $('#session-screen').addClass('screen-inactive');
-    });
+$('button.return-to-main').click(function() {
+    $('#main-screen').removeClass('screen-inactive');
+    $('#session-screen').addClass('screen-inactive');
 });
 
 /*
