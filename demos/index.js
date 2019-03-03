4b2c2fe44c5023d7bb859060ea9da2fbb0b5df73
veilance <christopher.tam@alumni.ubc.ca>
2019-03-03 10:14:19 -0800

    made modal

--- demos/index.js
+++ demos/index.js
@@ -1,5 +1,14 @@
+import { ModelTrainingYielder } from "@tensorflow/tfjs-layers/dist/base_callbacks";
+
 $('#start-session').on('click', function() {
     console.log('start-session: clicked');
     $('#main-screen').addClass('screen-inactive');
     $('#excerise-screen').removeClass('screen-inactive');
+
+
+
+
+    $('#exercise-1').click(function() {
+        $('#myModal').style.display = "block";
+    })
 });
